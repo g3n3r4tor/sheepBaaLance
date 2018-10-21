273549f80e09bccb16fce156d9df8202d11b8cc4
Jianfei <jiz2@hi.is>
2018-10-21 12:16:06 +0000

    Timer now listens to Sheep collision with Floor and stops Timer

--- Assets/Scripts/SheepAI.cs
+++ Assets/Scripts/SheepAI.cs
@@ -1,10 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class SheepAI : MonoBehaviour {
 
+	public event EventHandler FloorCollided;
+
 	public float MadnessTimer;			// The threshold in seconds when the sheep starts going crazy
+	public Tilemap floor;
 
 	// Use this for initialization
 	void Start () {
@@ -14,5 +19,17 @@ public class SheepAI : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if(col.gameObject == floor.gameObject)
+		{
+			if (FloorCollided != null)
+			{
+				FloorCollided(this, EventArgs.Empty);
+			}
+			// Application.LoadLevel(Application.loadedLevel);
+		}
 	}
 }
--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,16 +8,27 @@ public class Timer : MonoBehaviour {
 
 	private float lifeTime = 0.0f;		// Total seconds elapsed since Sheep has been in the game
 	private Text displayText;
+	private bool isAlive = true;
+	
+	public GameObject sheep;
 
 	// Use this for initialization
 	void Start () {
 		displayText = GetComponent<Text>();
+
+		sheep.GetComponent<SheepAI>().FloorCollided += OnFloorCollided;
+	}
+
+	void OnFloorCollided(object sender, EventArgs e) {
+		isAlive = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		lifeTime += Time.deltaTime;
-		displayText.text = FormatTime();
+		if (isAlive) {
+			lifeTime += Time.deltaTime;
+			displayText.text = FormatTime();
+		}
 	}
 
 	string FormatTime() {

6a9e9de00ceaaeb28356d4b795030b5781d4b14c
Ingimar Svanberg Jóhannesson <ingimar.johannesson@gmail.com>
2018-10-21 05:14:42 +0000

    Move my scene into a work folder and got a copy of the script files

---
+++ Assets/IngimarWorking/Scripts/SheepAI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepAI : MonoBehaviour {
+
+	public float MadnessTimer;			// The threshold in seconds when the sheep starts going crazy
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
